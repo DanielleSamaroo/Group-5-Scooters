4a6fdea3d4eec1286fe4efda2d6f7e5980feb396
Juan Postiglione <juan.postiglione@ufl.edu>
2024-07-11 21:10:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include "Group5ScootersProject.h"
-#include <QtWidgets/QApplication>
+#include "mainwindow.h"
+
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Group5ScootersProject w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
