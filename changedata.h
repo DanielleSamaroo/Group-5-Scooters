eec00b564bffdef8fd390f82b4bc9f0db0e101fa
Juan Postiglione <juan.postiglione@ufl.edu>
2024-08-05 12:49:56 -0400

    Create changedata.h

---
+++ changedata.h
@@ -0,0 +1,36 @@
+#ifndef CHANGEDATA_H
+#define CHANGEDATA_H
+
+#include <QDialog>
+#include <QtSql>
+#include <QDebug>
+
+
+namespace Ui {
+class changeData;
+}
+
+class changeData : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit changeData(QWidget *parent = nullptr);
+    ~changeData();
+
+private slots:
+    void on_buttonBox_accepted();
+
+    void on_toolButton_2_clicked();
+
+    void on_comboBox_currentIndexChanged(int index);
+
+    void on_lineEdit_editingFinished();
+
+    void on_buttonBox_rejected();
+
+private:
+    Ui::changeData *ui;
+};
+
+#endif // CHANGEDATA_H
